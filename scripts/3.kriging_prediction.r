f92afdebc9062a0208c83d7a17812532cc843a4c
sebastianguinazu <sebastianguinazu@gmail.com>
2021-05-21 23:11:36 -0300

    notas para paso 3

--- scripts/3.kriging_prediction.r
+++ scripts/3.kriging_prediction.r
@@ -1,4 +1,7 @@
 
+# cosas a probar:
 
-
-# levanto data 
+# kriging con los modelos del script2
+# performance predictiva de esos modelos en el dataset validacion
+# probar opcion de estimar variogramas segun performance en validacion
+# performance predictiva de variogramas estimados en paso anterior
